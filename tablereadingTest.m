6b24d70e301d877aa24d0ac312914d6b166c8350
ANESTHESIOLOGY\smgrady <smgrady@wisc.edu>
2025-10-03 16:34:57 -0500

    Changed the test file to operate as a batch script on all the files in the list.

--- tablereadingTest.m
+++ tablereadingTest.m
@@ -1,19 +1,40 @@
 % **** outline of mouse table curating we need to perform ****
 % 1. load in the "table of tables" so we know what to operate on
-% 2. for each table, load it in with appropriate opts - this is proving
-%   challenging because columns might be formatted as doubles or whatever.
-% 3. on the find the specific columns of interest, in case there's trouble (they don't all start at the same row)
-% 4. load in the specific columns and create the counts
-% ... do we want to edit and rewrite from here?
+% 2. use readAndCombineXlsxRecord 
+% 2a. for each sheet, load it in with appropriate opts
+% 2b. find the canonical columns and rows of interest (they don't all start in the same location!)
+% 2c. merge what we found from each sheet
+% 3. step through each sheet and merge!
 
-%
-xlsxFileName = 'Z:\PearceLabRecords\Mouse Inventory\Lamp5-cre\Lamp5-cre.xlsx'; % we'll step through a list of these, but start here for testing
 keyColumnHeaders = {'ID Number','DOB','Date of Exp','mouseAssignment','sacCode','fundingID'};
-saveFileName = 'Z:\PearceLabRecords\Mouse Inventory\Lamp5-cre\mouseCount.xlsx';
+bigSaveFile = 'Z:\PearceLabRecords\Mouse Inventory\2025totalMouseCount.xlsx';
 
+% example single animal file save
+% xlsxFileName = 'Z:\PearceLabRecords\Mouse Inventory\Lamp5-cre\Lamp5-cre.xlsx'; 
+% singleAnimalSaveFileName = 'Z:\PearceLabRecords\Mouse Inventory\Lamp5-cre\mouseCount.xlsx';
 
-% our new function
-[fullAnimalTable] = readAndCombineXlsxRecord(xlsxFileName,keyColumnHeaders);
+tableOfTablesFileName = 'Z:\PearceLabRecords\Mouse Inventory\InventorySummaryTest.xlsx';
+opts = detectImportOptions(tableOfTablesFileName,'Sheet','Lines and paths');
+opts.VariableNamingRule = 'preserve'; 
+tableOfTables = readtable(tableOfTablesFileName,opts);
+% loop through our list of tables!
+bigTable = table;
+for i = 1:size(tableOfTables,1)
+    xlsxFileName = tableOfTables.("Full path"){i};
+    thisLine = tableOfTables.("shorthand"){i};
+    try
+        disp(['Reading in ' thisLine]);
+        % our new function
+        [singleAnimaltable] = readAndCombineXlsxRecord(xlsxFileName,keyColumnHeaders);
+        % writetable(singleAnimaltable,singleAnimalSaveFileName);
+        bigTable = vertcat(bigTable,singleAnimaltable);
+        disp(['successfully read in in ' thisLine]);
+    catch
+        disp(['failed to read ' thisLine]);
+    end
+end
+
+writetable(bigTable,bigSaveFile);
 
 % cleanup here?
 % some of the rows we found (like the ones at the end or
@@ -21,6 +42,7 @@ saveFileName = 'Z:\PearceLabRecords\Mouse Inventory\Lamp5-cre\mouseCount.xlsx';
 % eliminateThese = ismissing(fullAnimalTable.("mouseAssignment"));
 %fullAnimalTable(eliminateThese,:) = [];
 
-writetable(fullAnimalTable,saveFileName);
+
+
 
 
